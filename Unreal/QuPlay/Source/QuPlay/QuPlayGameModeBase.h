e27d39cfc06b70b735f1fa92fb6c5331bc5512a7
Claudio Silva - AlienWare <ddk.claudio@gmail.com>
2017-12-11 14:34:26 -0200

    Project UE4.8 in cpp using Simtools plugin

---
+++ Unreal/QuPlay/Source/QuPlay/QuPlayGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuPlayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUPLAY_API AQuPlayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
